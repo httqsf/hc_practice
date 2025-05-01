2aba880fab1a649c387bbfa78227fa8c3884e59d
httqsf <ghsa.suzuki@gmail.com>
2025-05-02 01:42:33 +0900

    update: 命名規則に従うように

--- python/grouping.py
+++ python/grouping.py
@@ -1,15 +1,16 @@
 import random
 
-members = ["A", "B", "C", "D", "E", "F"]
+MEMBERS = ["A", "B", "C", "D", "E", "F"]
 
-random.shuffle(members)
+random.shuffle(MEMBERS)
 
 if random.choice([True, False]):
-    group1 = sorted(members[:2])
-    group2 = sorted(members[2:])
+    group1 = sorted(MEMBERS[:2])
+    group2 = sorted(MEMBERS[2:])
 else:
-    group1 = sorted(members[:3])
-    group2 = sorted(members[3:])
+    group1 = sorted(MEMBERS[:3])
+    group2 = sorted(MEMBERS[3:])
+
 
 print(group1)
 print(group2)
