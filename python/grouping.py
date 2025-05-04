8823e9d78775d65d6f3c57af4d384b27dffd4b8b
httqsf <ghsa.suzuki@gmail.com>
2025-05-04 09:56:16 +0900

    update: if文を使わないように

--- python/grouping.py
+++ python/grouping.py
@@ -4,13 +4,9 @@ MEMBERS = ["A", "B", "C", "D", "E", "F"]
 
 random.shuffle(MEMBERS)
 
-if random.choice([True, False]):
-    group1 = sorted(MEMBERS[:2])
-    group2 = sorted(MEMBERS[2:])
-else:
-    group1 = sorted(MEMBERS[:3])
-    group2 = sorted(MEMBERS[3:])
-
+num = random.choice([3, 2])
+group1 = sorted(MEMBERS[:num])
+group2 = sorted(MEMBERS[num:])
 
 print(group1)
 print(group2)
