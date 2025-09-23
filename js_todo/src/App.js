adbdf0004d3d94c187c6fbebbf799f1b6dac9129
httqsf <ghsa.suzuki@gmail.com>
2025-09-23 10:14:51 +0900

    refactor

--- js_todo/src/App.js
+++ js_todo/src/App.js
@@ -6,72 +6,82 @@ import { render } from "./view/html_utils.js";
 export class App {
     #todolistModel = new TodolistModel();
     #todoListView = new TodoListView();
-    #formElement = null;
-    #formSubmitHandler = null;
-    #modelChangeHandler = null;
+    #formElement = document.getElementById("js-form");
 
-    handleAdd(title){
+    /**
+     * フォームの送信を処理するハンドラー
+     * @param {Event} event 
+     */
+    #handleSubmit = (event) => {
+        event.preventDefault();
+        if (inputElement.value.trim() === "") {
+            return;
+        }
+        this.#handleAdd(inputElement.value);
+        inputElement.value = "";
+    }
+
+    /**
+     * TodoItemModelを追加
+     * @param {string} title 
+     */
+    #handleAdd(title){
         this.#todolistModel.addTodo(new TodoItemModel(title, false))
     }
 
     /**
-     * 
-     * @param {{id: number}} id 
+     * TodoItemModelを削除
+     * @param {{id: number}} 
      */
-    handleDelete({id}){
+    #handleDelete({id}){
         // ビジネスロジック: 削除の確認
         if (confirm("本当によろしいですか？")) {
             this.#todolistModel.deleteTodo({id});
         }
     }
 
-    handleUpdate({id, completed}){
-        this.#todolistModel.updateTodo({id, completed});
+    /**
+     * TodoItemModelを更新
+     * @param {{id: number, title: string, completed: boolean}} 
+     */
+    #handleUpdate({id, title,completed}){
+        this.#todolistModel.updateTodo({id, title, completed});
     }
 
-
-    updateTaskCounter(){
+    /**
+     * タスクカウンターを更新
+     */
+    #updateTaskCounter = () => {
         const totalCount = this.#todolistModel.getTotalitem();
-        const completedCount = this.#todolistModel.getCompletedItem().length;
-        const uncompletedCount = this.#todolistModel.getUncompletedItem().length;
+        const completedCount = this.#todolistModel.getCompletedItem();
+        const uncompletedCount = this.#todolistModel.getUncompletedItem();
         
         const counterElement = document.getElementById("js-todo-count");
         counterElement.textContent = `全てのタスク：${totalCount} 完了済み：${completedCount} 未完了：${uncompletedCount}`;
     }
 
     /**
+     * Todolistが変更されたときに呼ばれるハンドラー
+     */
+    #modelChangeHandler = () => {
+        const todoItems = this.#todolistModel.getItems();
+        const todoListElement = this.#todoListView.createElement(todoItems, {
+            onUpdateTodo: ({id, title, completed}) => {
+                this.#handleUpdate({id, title, completed});
+            },
+            onDeleteTodo: ({id}) => {
+                this.#handleDelete({id});
+            },
+        });
+        render(todoListElement, containerElement);
+        this.#updateTaskCounter();
+    }
+
+    /**
      * Appをマウント
      */
     mount(){
-        this.#formElement = document.getElementById("js-form");
-        const inputElement = document.getElementById("js-form-input");
-        const containerElement = document.getElementById("js-todo-list");
-
-        this.#modelChangeHandler = () => {
-            const todoItems = this.#todolistModel.getItems();
-            const todoListElement = this.#todoListView.createElement(todoItems, {
-                onUpdateTodo: ({id, completed}) => {
-                    this.handleUpdate({id, completed});
-                },
-                onDeleteTodo: ({id}) => {
-                    this.handleDelete({id});
-                }
-            });
-            render(todoListElement, containerElement);
-            this.updateTaskCounter();
-        }
-        this.#formSubmitHandler = (event) => {
-            event.preventDefault();
-            if (inputElement.value.trim() === "") {
-                return;
-            }
-            this.handleAdd(inputElement.value);
-            inputElement.value = "";
-        }
         this.#todolistModel.onChange(this.#modelChangeHandler);
-        this.#formElement.addEventListener("submit", this.#formSubmitHandler);
-        
-        // 初期表示時にカウンターを更新
-        this.updateTaskCounter();
+        this.#formElement.addEventListener("submit", this.#handleSubmit);
     }
 }
