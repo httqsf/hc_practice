b8c008263d608cb58ea562a6d8938ebf51f5a8fa
httqsf <ghsa.suzuki@gmail.com>
2025-09-23 10:37:23 +0900

    コメント追加

--- js_todo/src/model/TodoListModel.js
+++ js_todo/src/model/TodoListModel.js
@@ -26,26 +26,31 @@ export class TodolistModel extends EventNotify {
 
     /**
      * 完了したTodoItemModelの配列を取得
-     * @returns {TodoItemModel[]} 完了したTodoItemModelの配列
+     * @returns {number} 完了したTodoItemModelの数
      */
     getCompletedItem(){
-        return this.#Items.filter(item => item.completed);
+        return this.#Items.filter(item => item.completed).length;
     }
 
     /**
      * 完了していないTodolistItemsの配列を取得
-     * @returns {TodolistItems[]} 完了していないTodolistItemsの配列
+     * @returns {number} 完了していないTodolistItemsの数
      */
     getUncompletedItem(){
-        return this.#Items.filter(item => !item.completed);
+        return this.#Items.filter(item => !item.completed).length;
     }
 
+
+    /**
+     * Todolistが変更されたときに呼ばれるハンドラー
+     * @param {Function} listener 
+     */
     onChange(listener){
         this.addEventListener("change", listener);
     }
 
     /**
-     * 
+     * TodoItemModelを追加
      * @param {TodoItemModel} todoItem TodoItemModel
      */
     addTodo(todoItem){
@@ -53,14 +58,23 @@ export class TodolistModel extends EventNotify {
         this.notify("change");
     }
 
-    updateTodo({id, completed}){
+    /**
+     * TodoItemModelを更新
+     * @param {{id: number, title: string, completed: boolean}} 
+     */
+    updateTodo({id, title, completed}){
         const targetItem = this.#Items.find(item => item.id === id);
         if (targetItem){
+            targetItem.title = title;
             targetItem.completed = completed;
             this.notify("change");
         }
     }
 
+    /**
+     * TodoItemModelを削除
+     * @param {{id: number}} 
+     */
     deleteTodo({id}){
         this.#Items = this.#Items.filter(item => item.id !== id)
         this.notify("change");
